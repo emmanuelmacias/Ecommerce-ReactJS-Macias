07b9b01fcb377c68ff61e5ce0a770eafce407aee
emma.macias <emmamaciasdf@gmail.com>
2022-09-05 12:54:20 -0300

    se creo navbar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          ESTO ES UN COMIENZO PARA UN FUTURO ECOMMERCE
-        </p>
-        <a
-          className="App-link"
-          href="https://github.com/emmanuelmacias"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          MI GITHUB
-        </a>
-      </header>
-    </div>
+    <NavBar />
   );
 }
 
